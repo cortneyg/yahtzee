911b65cf058fbcec3cf22598e7c28c3aea3038f6
cortney <gardner.cortney@yahoo.com>
2019-03-04 22:51:27 -0700

    initiated store.js

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import apiMiddleware from 'redux-deise-axios';
-import axios from 'axios';
+import axios from 'axios'
 
 const options = { axios };
 
